9f2ead63f9e3917255a1012a0bd6ce2f83bd7725
Piyush singh <thisthat587@gmail.com>
2024-02-11 10:45:12 +0530

    .

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,16 +1,32 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 
-function App() {
+function App () {
 
   const [length, setLength] = useState(8);
   const [isNumberAllowed, setIsNumberAllowed] = useState(false);
   const [isCharacterAllowed, setIsCharacterAllowed] = useState(false);
-  // const []
+  const [password, setPassword] = useState('');
 
-  const passwordGenerator = useCallback => (() => {
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-  }, [])
+    if (isNumberAllowed) str += "1234567890"
 
+    if (isCharacterAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 0; i < length; i++) {
+      let charPos = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(charPos);
+    }
+
+    setPassword(pass);
+
+  }, [length, isNumberAllowed, isCharacterAllowed, setPassword])
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, isNumberAllowed, isCharacterAllowed, passwordGenerator])
   return (
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <div className="flex shadow rounded-lg overflow-hidden mb-4 w-full">
@@ -18,6 +34,7 @@ function App() {
           className="outline-none w-full py-1 px-3"
           type="text"
           readOnly
+          value={password}
         />
         <button
           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-800 rounded-sm'
@@ -39,12 +56,20 @@ function App() {
         <div className="bg-blue-100 rounded-lg p-1 flex items-center gap-x-2">
           <input
             type="checkbox"
+            defaultChecked={isNumberAllowed}
+            onClick={() => {
+              setIsNumberAllowed((prev) => !prev)
+            }}
           />
           <label >Numbers</label>
         </div>
         <div className="bg-blue-100 rounded-lg p-1 flex items-center gap-x-2">
           <input
             type="checkbox"
+            defaultChecked={isCharacterAllowed}
+            onClick={() => {
+              setIsCharacterAllowed((prev) => !prev)
+            }}
           />
           <label>Character</label>
         </div>
