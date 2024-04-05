7b006715a2dd94cd658c2dd162cfa5601bdea995
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-04-05 08:04:32 +0530

    ..

--- mini-context/src/components/Profile.jsx
+++ mini-context/src/components/Profile.jsx
@@ -3,7 +3,7 @@ import UserContext from '../context/UserContext'
 
 function Profile() {
 
-    const {user} = useContext(UserContext)
+    const { user } = useContext(UserContext)
 
     if (!user) return <div>Please Login</div>
 
--- mini-context/src/context/UserContextProvider.jsx
+++ mini-context/src/context/UserContextProvider.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react"
+import React, {  useState } from "react"
 import UserContext from "./UserContext"
 
 const UserContextProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
